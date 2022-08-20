1505fa4580ac4075e16b49a4058024f9ae32f27c
globaltempest <ericyifwang@gmail.com>
2022-08-20 12:26:45 -0400

    added font directory, changed title font

--- App.js
+++ App.js
@@ -1,18 +1,32 @@
 import React from "react";
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button, Alert, Image } from 'react-native';
+import { useFonts } from "expo-font";
 
 export default function App() {
+
+  const [loaded] = useFonts({
+    Montserrat: require('./assets/fonts/Montserrat-VariableFont_wght.ttf'),
+    NunitoSans: require('./assets/fonts/NunitoSans-Black.ttf'),
+    RaleWay: require('./assets/fonts/Raleway-VariableFont_wght.ttf'),
+  }) 
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
 
-      <Text style={{color: 'red', fontWeight: 'bold', fontSize: 50,}}>FightBite</Text>
+      <Text style={{color: 'red', fontSize: 50, fontFamily: 'RaleWay',}}> FightBite</Text>
       <Image height source={require('./assets/logo.png')} style= {{width: 300, height: 300}}/>
       <StatusBar style="auto"/>
       <Button
         title="Start"
-        onPress={() => console.log("yes")}
+        onPress={() => console.log("yes")}    
       />
+
+      
 
     </View>
   );
