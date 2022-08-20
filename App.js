759537620aec27135adf62997103fa3ce8b3b990
RaymondMei <raymondmei.rm@gmail.com>
2022-08-19 20:40:37 -0400

    init FightBite
    Co-authored-by: globaltempest <globaltempest@users.noreply.github.com>
    Co-authored-by: ebwu95 <ebwu95@users.noreply.github.com>

---
+++ App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>asdf</Text>
+      <StatusBar style="auto" />
+      <Button
+        title="Press me"
+        onPress={() => Alert.alert('Simple Button pressed')}
+      />
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
