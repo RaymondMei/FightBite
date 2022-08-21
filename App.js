f6c69326bfa48296a501741950beca8d04e11aad
RaymondMei <raymondmei.rm@gmail.com>
2022-08-21 03:39:35 -0400

    CONNECT FRONT END TO BACKEND WITH API

--- App.js
+++ App.js
@@ -20,6 +20,8 @@ import { useFonts } from "expo-font";
 import { Camera } from 'expo-camera';
 import * as MediaLibrary from 'expo-media-library';
 import * as ImagePicker from 'expo-image-picker';
+import { Buffer } from 'buffer';
+import { withSafeAreaInsets } from 'react-native-safe-area-context';
 
 
 
@@ -53,6 +55,10 @@ export default function App() {
           name="GetPhoto"
           component={GetPhotoScreen}
         />
+        <Stack.Screen
+          name="Results"
+          component={ResultsScreen}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -65,28 +71,32 @@ function StartScreen({ navigation }) {
 
       <LinearGradient
         // Background Linear Gradient
-        colors={['#FFB88C', '#DE6262', `#FFB88C`, 'transparent']}
+        colors={['#FE6197', '#FFB463']}
         style={styles.background}
-
       />
-
-      <Text style={{ color: 'black', fontSize: 70, fontFamily: 'RobotoCondensedBold',}}> FightBite</Text>
-      <Image height source={require('./assets/logo.png')} style={{ width: 400, height: 400 }} />
+      <Text style={{ color: 'black', fontSize: 70, fontFamily: 'RobotoCondensedBold', }}> FightBite</Text>
+      <Image height source={require('./assets/logo.png')} style={{
+        width: 400, height: 400, shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.8,
+        shadowRadius: 2,
+      }} />
       <StatusBar style="auto" />
-      <Button
-        title="Add Photo from Library"
-        onPress={() => {
+      <LinearGradient
+        colors={['#1b1c27', '#424454']}
+        start={{ x: 0, y: 0 }} end={{ x: 1, y: 0 }} style={[styles.button, { padding: 25 }]}>
+        <TouchableOpacity onPress={() => {
           navigation.navigate('GetPhoto');
           console.log("start button clicked");
-        }}
-        styles={styles.button}
-      />
-
-    </SafeAreaView>
+        }} >
+          <Text style={{ color: 'white', fontSize: 35, fontFamily: 'RobotoCondensedBold' }}> Identify Bite</Text>
+        </TouchableOpacity>
+      </LinearGradient>
+    </SafeAreaView >
   );
 }
 
-function GetPhotoScreen() {
+function GetPhotoScreen({ navigation }) {
 
   let cameraRef = useRef();
   const [hasCameraPermission, setHasCameraPermission] = useState();
@@ -101,9 +111,37 @@ function GetPhotoScreen() {
     })();
   }, []);
 
-  let scanPhoto = async () => {
+  async function scanPhoto(photo) {
+    console.log(photo.uri);
+    async function uploadImageAsync(uri) {
+      let apiUrl = 'https://fitebite.herokuapp.com/predict';
 
+      let uriParts = uri.split('.');
+      let fileType = uri[uri.length - 1];
+
+      let formData = new FormData();
+      formData.append('file', {
+        uri,
+        name: `photo.${fileType}`,
+        type: `image/${fileType}`,
+      });
+
+      let options = {
+        method: 'POST',
+        body: formData,
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'multipart/form-data',
+        },
+      };
+
+      return fetch(apiUrl, options);
+    }
+    let uploadResponse = await uploadImageAsync(photo.uri);
+    let uploadResult = await uploadResponse.json();
+    navigation.navigate("Results", { results: JSON.stringify(uploadResult) });
   }
+
 
   let capturePic = async () => {
     let options = {
@@ -119,6 +157,7 @@ function GetPhotoScreen() {
     let newPhoto = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
       allowsEditing: true,
+      allowsMultipleSelection: false,
       aspect: [1, 1],
       quality: 1,
       base64: true,
@@ -126,6 +165,7 @@ function GetPhotoScreen() {
     });
 
     if (!newPhoto.cancelled) {
+      // console.log(newPhoto);
       setPhoto(newPhoto);
     }
   }
@@ -139,44 +179,124 @@ function GetPhotoScreen() {
 
     return (
       <SafeAreaView style={styles.photosContainer}>
-        {photo && <Image style={styles.camera} source={{ uri: "data:image/jpg;base64," + photo.base64 }} />}
-        {/* {photo && <Image source={{ uri: photo }} />} */}
+        <LinearGradient
+          // Background Linear Gradient
+          colors={['#FE6197', '#FFB463']}
+          style={{
+            position: 'absolute',
+            left: 0,
+            right: 0,
+            top: 0, height: '130%', flex: 1, alignItems: 'center'
+          }}
+        >
+          <View style={{ position: 'absolute', top: '15%', height: 500, justifyContent: 'space-between' }}>
 
-        <View style={styles.capturedPhotoButtons}>
-          {hasMediaLibraryPermission ? <TouchableOpacity onPress={scanPhoto} style={styles.button}>
-            <Text>Scan</Text>
-          </TouchableOpacity> : undefined}
-          {hasMediaLibraryPermission ? <TouchableOpacity onPress={savePhoto} style={styles.button}>
-            <Text>Save</Text>
-          </TouchableOpacity> : undefined}
-          <TouchableOpacity onPress={() => setPhoto(undefined)} style={styles.button}>
-            <Text>Discard</Text>
-          </TouchableOpacity>
-        </View>
+            <View style={{ height: 365, justifyContent: 'space-between' }}>
+              {photo && <Image style={styles.camera} source={{ uri: "data:image/png;base64," + photo.base64 }} />}
+              {hasMediaLibraryPermission ?
+                <LinearGradient
+                  colors={['#6448FE', '#5FC6FF']}
+                  start={{ x: 0, y: 0 }} end={{ x: 1, y: 0 }}
+                  style={[styles.button, { height: 60 }]}>
+                  <TouchableOpacity onPress={async () => { await scanPhoto(photo); }}>
+                    <Text>Scan</Text>
+                  </TouchableOpacity>
+                </LinearGradient>
+                : undefined}
+            </View>
+            <View style={[styles.capturedPhotoButtons]}>
+              {hasMediaLibraryPermission ?
+                <LinearGradient
+                  colors={['#FFFE6197', '#FFB463']}
+                  start={{ x: 0, y: 0 }} end={{ x: 1, y: 0 }}
+                  style={[styles.button, { width: '45%', height: 60 }]}>
+                  <TouchableOpacity onPress={() => { savePhoto(photo); }}>
+                    <Text>Save</Text>
+                  </TouchableOpacity>
+                </LinearGradient>
+                : undefined}
+              <LinearGradient
+                colors={['#FF5DCD', '#FF8484']}
+                start={{ x: 0, y: 0 }} end={{ x: 1, y: 0 }}
+                style={[styles.button, { width: '45%', height: 60 }]}>
+                <TouchableOpacity onPress={() => setPhoto(undefined)}>
+                  <Text>Discard</Text>
+                </TouchableOpacity>
+              </LinearGradient>
+            </View>
 
+          </View>
+        </LinearGradient>
       </SafeAreaView>
     );
   }
 
   return (
-    <SafeAreaView style={styles.photosContainer}>
-      <Camera
-        style={styles.camera}
-        ref={cameraRef}
-        ratio="1:1">
-        <StatusBar style="auto" />
-      </Camera>
-      <View>
-        <TouchableOpacity onPress={capturePic} style={styles.button}>
-          <Text>Capture</Text>
-        </TouchableOpacity>
-      </View>
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity onPress={pickImage} style={styles.button}>
-          <Text>Pick from Gallery</Text>
-        </TouchableOpacity>
-      </View>
+    <SafeAreaView style={styles.container}>
+      <LinearGradient
+        // Background Linear Gradient
+        colors={['#FE6197', '#FFB463']}
+        style={{
+          position: 'absolute',
+          left: 0,
+          right: 0,
+          top: 0, height: '130%', flex: 1, alignItems: 'center',
+        }}
+      >
+        <View style={{ alignItems: 'center', position: 'absolute', top: '10%', justifyContent: 'space-between', height: 380 }}>
+          <Camera
+            style={[styles.camera, { borderColor: 'black', borderWidth: 3 }]}
+            ref={cameraRef}
+            ratio="1:1">
+            <StatusBar style="auto" />
+          </Camera>
 
+          <LinearGradient
+            colors={['#1b1c27', '#424454']}
+            start={{ x: 0, y: 0 }} end={{ x: 1, y: 0 }}
+            style={[styles.button, { width: 170 }]}>
+            <TouchableOpacity onPress={capturePic} style={styles.button}>
+              <Text style={{ color: 'white', fontSize: 25, fontFamily: 'RobotoCondensedBold' }}>Capture</Text>
+            </TouchableOpacity>
+          </LinearGradient>
+        </View>
+
+        <View style={{ width: 300, position: 'absolute', bottom: '35%', alignItems: 'center', }}>
+          <LinearGradient
+            colors={['#1b1c27', '#424454']}
+            start={{ x: 0, y: 0 }} end={{ x: 1, y: 0 }}
+            style={[styles.button, { width: 300 }]}>
+            <TouchableOpacity onPress={pickImage} style={styles.button}>
+              <Text style={{ color: 'white', fontSize: 25, fontFamily: 'RobotoCondensedBold' }}>Pick from Gallery</Text>
+            </TouchableOpacity>
+          </LinearGradient>
+        </View >
+      </LinearGradient>
+    </SafeAreaView >
+  );
+}
+
+function ResultsScreen({ route }) {
+  const { results } = route.params;
+  let data = JSON.parse(results);
+  console.log(data.class);
+  return (
+    <SafeAreaView style={styles.container}>
+      <LinearGradient
+        // Background Linear Gradient
+        colors={['#FE6197', '#FFB463']}
+        style={{
+          position: 'absolute',
+          left: 0,
+          right: 0,
+          top: 0, height: '130%', flex: 1, alignItems: 'center',
+        }}
+      >
+
+        <Text style={{ position: 'absolute', top: '30%', color: 'black', fontSize: 70, fontFamily: 'RobotoCondensedBold', }}>
+          {data.class}
+        </Text>
+      </LinearGradient>
     </SafeAreaView>
   );
 }
@@ -197,20 +317,23 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   button: {
-    minHeight: 40,
+    minHeight: 50,
     minWidth: 80,
-    backgroundColor: 'salmon',
     borderRadius: 7,
     justifyContent: 'center',
     alignItems: 'center',
-
+    padding: 10,
+    color: 'white',
+  },
+  lingrad: {
+    flex: 1,
   },
   background: {
     position: 'absolute',
     left: 0,
     right: 0,
     top: 0,
-    height: 1000,
+    height: "130%",
   },
   photosContainer: {
     flex: 1,
